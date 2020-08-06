dbeef2bc39abbf0f6c40c74d5b67c0189ee4d90a
Patryk Jary <patrykjary123@gmail.com>
2020-08-06 18:50:31 +0200

    no message

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let navigation=document.querySelector(".icofont-navigation-menu");
 let mbBarUl=document.querySelector(".mb-bar-ul");
-let allContent=document.querySelector('.all-content');
+let bgnr1=document.querySelector('.background.nr1');
+let bgnr2=document.querySelector('.background.nr2');
+let bgnr3=document.querySelector('.background.nr3');
 
 
 
@@ -30,3 +32,14 @@ navigation.addEventListener('click',()=>{
     return 0;
   }
 });
+
+const bantl=new TimelineMax({repeat:-1,repeatDelay:3,delay:3});
+  bantl.fromTo(bgnr1,0.5,{delay:3,left:"-100%"},{left:0});
+  bantl.to(bgnr1,0.5,{delay:6,left:"100%"});
+  bantl.to(bgnr1,0,{left:"-100%"});
+  bantl.fromTo(bgnr2,0.5,{left:"-100%"},{left:0},"-=0.5");
+  bantl.to(bgnr2,0.5,{delay:6,left:"100%"});
+  bantl.to(bgnr2,0,{left:"-100%"});
+  bantl.fromTo(bgnr3,0.5,{left:"-100%"},{left:0},"-=0.5");
+  bantl.to(bgnr3,0.5,{delay:6,left:"100%"});
+  bantl.to(bgnr3,0,{left:"-100%"});
